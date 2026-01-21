d32883644c904d1c0942b1ab11ac8ec76420f108
RAMOx99 <omarabdelghany177@gmail.com>
2026-01-21 22:00:51 +0200

    feat(effects): add GlobalEffects component

---
+++ src/components/effects/GlobalEffects.tsx
@@ -0,0 +1,19 @@
+'use client';
+
+import CosmicBackground from '@/components/effects/CosmicBackground';
+
+/**
+ * Global Effects Provider
+ * Renders cosmic background effects across all pages
+ */
+export default function GlobalEffects() {
+    return (
+        <CosmicBackground
+            enableStars={true}
+            enableOrbs={true}
+            enableMeteors={false}
+            enableGrid={false}
+            enableGlowCursor={false}
+        />
+    );
+}
