7ba1433038e011b836a8bfa1d805247c43a7a8ee
RAMOx99 <omarabdelghany177@gmail.com>
2026-01-21 22:02:22 +0200

    docs: add DeleteConfirmModal docs

--- src/components/ui/DeleteConfirmModal.tsx
+++ src/components/ui/DeleteConfirmModal.tsx
@@ -1,3 +1,4 @@
+// DeleteConfirmModal - Confirmation dialog for deletions
 'use client';
 
 import { useState } from 'react';
@@ -63,3 +64,4 @@ export default function DeleteConfirmModal({
         </div>
     );
 }
+
